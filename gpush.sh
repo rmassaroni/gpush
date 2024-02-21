e041313e5ca024c3296886c18f6543e0f4f6719f
rjm <robjmass@gmail.com>
2024-02-20 23:32:19 -0500

    {1:-'unnamed commit'}

--- gpush.sh
+++ gpush.sh
@@ -1,7 +1,8 @@
 gpush() {
     if [ -d .git ]; then
         message="{1:-'unnamed commit'}"
-        git add --all && git commit -m "$message" && git push -u origin main
+        echo "pushing '$message' to main"
+        #git add --all && git commit -m "$message" && git push -u origin main
     else
         echo "ERROR: Not a Git repository."
     fi
