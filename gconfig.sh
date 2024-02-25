b8805f17bf577a5d8595a1d4bb096212628ac74c
rjm <robjmass@gmail.com>
2024-02-24 20:15:02 -0500

    require_gcopies option

--- gconfig.sh
+++ gconfig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 auto_glink=true
+require_gcopies=false #require directory 'gcopies' to put copes in. if gcopies exists, copies will go there even if this is turned off
 
 gprint-settings() {
     echo "auto_glink: $auto_glink"
