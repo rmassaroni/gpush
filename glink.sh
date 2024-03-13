e382d282eb241d3be6ca5c103fdf0474cfbc1e84
rjm <robjmass@gmail.com>
2024-03-12 20:12:41 -0400

    unnamed commit

--- glink.sh
+++ glink.sh
@@ -7,7 +7,7 @@ glink() {
         echo "Directory 'glinks' found in current directory."
         gcopy ".glinks"
     else
-        echo "Directory 'glinks' not found."
+        echo "Directory '.glinks' not found."
         return 1
     fi
 }
