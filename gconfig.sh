455cbdb6e8776b981b57a647f7d9d4da8f56ff1f
rjm <robjmass@gmail.com>
2024-03-26 23:23:11 -0400

    gconfig settings

--- gconfig.sh
+++ gconfig.sh
@@ -3,6 +3,10 @@
 auto_glink=true
 require_gcopies=false #require directory 'gcopies' to put copes in. if gcopies exists, copies will go there even if this is turned off
 unhide=true #removes "." in copied files
+default_message="unnamed commit"
+default_branch="main"
+glink_dir="./"
+
 
 gprint-settings() {
     echo "auto_glink: $auto_glink"
