22b20b6abf3d544f051b2d06e176de1c51f29884
rjm <robjmass@gmail.com>
2024-04-05 16:44:17 -0400

    unnamed commit

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ -f "$HOME/gpush.sh" ]; then
+if [ -f "$HOME/.gpush/gpush" ]; then
     echo "gpush.sh already exists in the home directory."
     exit 1
 fi
 
 #curl -sSL -o "$HOME/gpush.sh" https://github.com/rmassaroni/gpush/gpush
+mkdir -p $HOME/.gpush
 git clone https://github.com/rmassaroni/gpush $HOME/.gpush
-chmod +x $HOME/gpush.sh
-echo "export PATH=\"\$HOME:\$PATH\"" >> "$HOME/.zshrc"
+chmod +x $HOME/.gpush/gpush
+echo "export PATH=\"\$HOME/.gpush:\$PATH\"" >> "$HOME/.zshrc"
 source "$HOME/.zshrc"
 
 echo "gpush has been installed to $HOME directory and added to the PATH."
