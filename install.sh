b9332f4aec41825fdc6f784a9cc96b548d569693
rjm <robjmass@gmail.com>
2024-04-05 16:17:21 -0400

    add to path

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-URL="https://github.com/rmassaroni/gpush/gpush"
+if [ -f "$HOME/gpush.sh" ]; then
+    echo "gpush.sh already exists in the home directory."
+    exit 1
+fi
 
-bash <(curl -s "$URL")
+curl -sSL -o "$HOME/gpush" https://github.com/rmassaroni/gpush/gpush
+echo "export PATH=\"\$HOME:\$PATH\"" >> "$HOME/.zshrc"
+source "$HOME/.zshrc"
+
+echo "gpush has been installed to $HOME directory and added to the PATH."
