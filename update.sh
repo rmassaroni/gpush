2b4007fd946bbd1f102ececcf19b6822c690a943
rjm <robjmass@gmail.com>
2024-04-06 13:24:32 -0400

    update

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Specify the path to the gpush.sh script
+gpush_path="$HOME/.gpush/gpush.sh"
+
+# Pull the latest changes from the git repository
+git -C "$HOME/.gpush" pull origin main
+
+# Make sure the gpush.sh script is executable
+chmod +x "$gpush_path"
+
+echo "gpush has been updated."
+
