94a90ad4270f83c23e4c8d3016ac03f2b4e432ea
rjm <robjmass@gmail.com>
2024-04-06 13:32:24 -0400

    unnamed commit

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@
 # Specify the path to the gpush.sh script
 gpush_path="$HOME/.gpush/gpush.sh"
 
+git -C "$HOME/.gpush" clean -df
 # Pull the latest changes from the git repository
 git -C "$HOME/.gpush" pull --force origin main
 
-git -C "$HOME/.gpush" clean -df
 
 # Make sure the gpush.sh script is executable
 chmod +x "$gpush_path"
