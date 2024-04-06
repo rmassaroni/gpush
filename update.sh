bb1a1047f1987e3f1aee2314d530b6108fc412a3
rjm <robjmass@gmail.com>
2024-04-06 13:34:41 -0400

    unnamed commit

--- update.sh
+++ update.sh
@@ -3,9 +3,16 @@
 # Specify the path to the gpush.sh script
 gpush_path="$HOME/.gpush/gpush.sh"
 
-git -C "$HOME/.gpush" clean -df
+#git -C "$HOME/.gpush" clean -df
 # Pull the latest changes from the git repository
-git -C "$HOME/.gpush" pull --force origin main
+#git -C "$HOME/.gpush" pull --force origin main
+
+# Pull the latest changes from the git repository, forcing the pull
+git -C "$HOME/.gpush" fetch --all
+git -C "$HOME/.gpush" reset --hard origin/main
+
+# Clean untracked files in the repository
+git -C "$HOME/.gpush" clean -df
 
 
 # Make sure the gpush.sh script is executable
