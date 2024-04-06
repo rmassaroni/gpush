80940173386f346c6770750c861a44cc94c8fb99
rjm <robjmass@gmail.com>
2024-04-06 13:27:06 -0400

    force pull update

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 gpush_path="$HOME/.gpush/gpush.sh"
 
 # Pull the latest changes from the git repository
-git -C "$HOME/.gpush" pull origin main
+git -C "$HOME/.gpush" pull --force --clean origin main
 
 # Make sure the gpush.sh script is executable
 chmod +x "$gpush_path"
