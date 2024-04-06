2194469fae27a5d5b03b3e88a71589aaaa874e81
rjm <robjmass@gmail.com>
2024-04-06 13:03:58 -0400

    quiet glink fix

--- glink.sh
+++ glink.sh
@@ -6,12 +6,19 @@ glink() {
     is_quiet() {
         [[ "$1" = -q ]]
     }
+    quiet=false
+    if is_quiet "$1"; then
+        quiet=true 
+        shift
+    fi
+    echo "$quiet"
+
 
     if [ -d ".glinks" ]; then
         echo "Directory 'glinks' found in current directory."
         gcopy ".glinks"
     else
-        if is_quiet "$1"; then
+        if [ "$quiet" = true ]; then
             echo "Directory '.glinks' not found."
         fi
         return 1
