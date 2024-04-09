4824e9564c9f62b8bb6a870644abe8335818a938
rjm <robjmass@gmail.com>
2024-04-09 11:41:09 -0400

    removed .git in clone

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ if [ -f "$HOME/.gpush/gpush.sh" ]; then
 else
     #curl -sSL -o "$HOME/gpush.sh" https://github.com/rmassaroni/gpush/gpush
     mkdir -p $HOME/.gpush
-    git clone https://github.com/rmassaroni/gpush $HOME/.gpush
+    #git clone https://github.com/rmassaroni/gpush $HOME/.gpush
+    git clone --depth 1 https://github.com/rmassaroni/gpush $HOME/.gpush
     chmod +x $HOME/.gpush/gpush.sh
     echo "export PATH=\"\$HOME/.gpush:\$PATH\"" >> "$HOME/.zshrc"
     source "$HOME/.zshrc"
