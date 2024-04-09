7c3aba78a2db328ceb39e024ff0b18dc4826f84f
rjm <robjmass@gmail.com>
2024-04-09 12:04:26 -0400

    ginit.sh

--- ginit.sh
+++ ginit.sh
@@ -1,4 +1,15 @@
 #!/bin/zsh
 
 
-ginit()
+ginit() {
+
+    current_directory=$(basename "$(pwd)")
+
+    git init
+    touch README.md
+    git branch -M main
+    git remote add origin https://github.com/user/$current_directory.git
+    git add .
+    git commit -m "first commit"
+    git push -u origin main
+}
