ffcbd5ec8f8f5c40860c3a490adac48594cd3a80
rjm <robjmass@gmail.com>
2024-04-09 12:08:12 -0400

    ginit.sh

--- ginit.sh
+++ ginit.sh
@@ -2,13 +2,23 @@
 
 
 ginit() {
+    if [ -d ".git" ]; then
+        echo "Error: already a git repo"
+        return 1
+    fi
+
+    if [ -z "$1" ]; then
+        echo "Please provide a username as an argument."
+        return 1
+    fi
+    username=$1
 
     current_directory=$(basename "$(pwd)")
 
     git init
     touch README.md
     git branch -M main
-    git remote add origin https://github.com/user/$current_directory.git
+    git remote add origin https://github.com/$username/$current_directory.git
     git add .
     git commit -m "first commit"
     git push -u origin main
